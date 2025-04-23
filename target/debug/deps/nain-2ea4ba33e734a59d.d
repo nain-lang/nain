efb3533f6ec38a523ead225ab774f37e867618a7
nebulae <mar1lusk1@proton.me>
2025-04-23 16:47:15 +0200

    Add SRC folder
    Turns out it was a sub-module

---
+++ target/debug/deps/nain-2ea4ba33e734a59d.d
@@ -0,0 +1,6 @@
+/home/mario/Projects/nain/target/debug/deps/libnain-2ea4ba33e734a59d.rmeta: src/main.rs src/lex.rs
+
+/home/mario/Projects/nain/target/debug/deps/nain-2ea4ba33e734a59d.d: src/main.rs src/lex.rs
+
+src/main.rs:
+src/lex.rs:
---
+++ target/debug/nain.d
@@ -0,0 +1,1 @@
+/home/mario/Projects/nain/target/debug/nain: /home/mario/Projects/nain/src/lex.rs /home/mario/Projects/nain/src/main.rs
